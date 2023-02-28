2cc38318156e2dbb09a3e57f6746964755c540c2
Anastasia Kuznetsova <asiakisa11@gmail.com>
2023-02-28 23:27:18 +0500

    new

---
+++ C#_HW_z41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+Console.Clear();
+Console.Write("Введите числа через пробел: ");
+String numb = Console.ReadLine();
+string[] Numbers = numb.Split(' ');
+int[] ConvertNumbers = new int[Numbers.Length];
+for (int i = 0; i < Numbers.Length; i++)
+{
+    ConvertNumbers[i] = Int32.Parse(Numbers[i]);
+}
+int GetCountOfPosNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count = count + 1;
+
+        }
+    }
+    return count;
+}
+Console.Write("Количество чисел больше 0: ");
+Console.WriteLine(GetCountOfPosNumbers(ConvertNumbers));
